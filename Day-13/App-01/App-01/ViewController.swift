7d8f16d4c7aae7223a3b0041bc1e1d826afb88a6
nhm1096 <nhm1096@gmail.com>
2018-01-18 09:16:04 +0700

    added Day-13 / App-01

---
+++ Day-13/App-01/App-01/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  App-01
+//
+//  Created by Nguyễn Minh on 18/01/2018.
+//  Copyright © 2018 Nguyễn Minh. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
