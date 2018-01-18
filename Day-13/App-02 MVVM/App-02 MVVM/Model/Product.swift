1830c42c1d722090157c8eeae992625ac03b5726
nhm1096 <nhm1096@gmail.com>
2018-01-18 17:32:14 +0700

    updated class Product

---
+++ Day-13/App-02 MVVM/App-02 MVVM/Model/Product.swift
@@ -0,0 +1,18 @@
+//
+//  Product.swift
+//  App-02 MVVM
+//
+//  Created by Nguyễn Minh on 18/01/2018.
+//  Copyright © 2018 Nguyễn Minh. All rights reserved.
+//
+
+import UIKit
+class Product {
+    var name: String
+    var price: Double
+    
+    init(name: String, price: Double) {
+        self.name = name
+        self.price = price
+    }
+}
